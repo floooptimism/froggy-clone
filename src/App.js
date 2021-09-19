e163ba5b059126f529526e0eca96355fbbe76c0a
floooptimism <jackmord445@gmail.com>
2021-09-19 17:43:26 +0800

    navbar

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ function App() {
 
   return (
     <div>
-      <div id="navbar" className="h-14 px-3 flex items-center justify-between absolute top-0 left-0 w-full">
-        <div id="navbar-menu" className="w-10 h-10 rounded-lg bg-white flex items-center justify-center">
+      <div id="navbar" className=" px-3 md:px-10 md:py-5 flex items-center justify-between absolute top-0 left-0 w-full">
+        <div id="navbar-menu" className="w-10 h-10 rounded-lg bg-white flex items-center justify-center md:hidden">
           <Hamburger 
             isOpen={navhamburgerIsOpen}
             menuClicked={()=>setNavhamburgerIsOpen(prev => !prev)}
@@ -21,12 +21,27 @@ function App() {
           />
         </div>
 
-        <div id="navbar-logo" className="">
-          <img src="/images/header-logo.svg" className="w-10"/>
+        <div id="navbar-logo" className="flex-1">
+          <img src="/images/header-logo.svg" className="w-10 md:w-20"/>
         </div>
 
-        <div id="navbar-wallet" className="w-10 h-10 rounded-lg bg-white">
+        {/* links */}
+        <div id="navbar-links" className="flex-1 hidden md:flex space-x-14 items-center justify-center text-xl">
+          <a href="#">Story</a>
+          <a href="#">Frogs</a>
+          <a href="#">Team</a>
+          <a href="#">Roadmap</a>
+          <a href="#">Faq</a>
+        </div>
 
+
+        <div id="navbar-wallet-sm" className="w-10 h-10 rounded-lg bg-white md:hidden">
+
+        </div>
+        <div id="navbar-wallet-md" className="flex-1 flex text-white">
+          <button className="w-44 h-16 border ml-auto" style={{'backgroundColor':'#01CA17'}}>
+            Connect
+          </button>
         </div>
       </div>
 
