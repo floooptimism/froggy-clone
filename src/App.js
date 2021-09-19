10ee70bc4c33391080f78a77de51552d5c7147e3
potchii <benitezernest33@gmail.com>
2021-09-19 17:48:31 +0800

    hero section

--- src/App.js
+++ src/App.js
@@ -30,8 +30,9 @@ function App() {
         </div>
       </div>
 
-      <div id="hero">
-        {/* image here */}
+      <div id="hero" className="flex justify-center items-center h-64">
+        <img src="/images/help-landing.png" width="600" height="208"/>
+
       </div>
 
       <div id="story">
