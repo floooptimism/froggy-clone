0ac98df00987c817826b8df6d0c489bc5a40ff20
floooptimism <jackmord445@gmail.com>
2021-09-19 20:30:32 +0800

    even more responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    screens: {
+      'mdp': '900px'
+    },
     extend: {},
   },
   variants: {
