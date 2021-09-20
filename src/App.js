343527fd8ace6f4db70984dc4f0b52679625f0eb
floooptimism <jackmord445@gmail.com>
2021-09-20 22:15:51 +0800

    responsive update

--- src/App.js
+++ src/App.js
@@ -7,9 +7,13 @@ import Hamburger from 'react-hamburger-menu'
 function App() {
   const [navhamburgerIsOpen, setNavhamburgerIsOpen] = useState(false);
 
+  window.addEventListener('scroll', function(e){
+    console.log(window.scrollY);
+  })  
+
   return (
     <div>
-      <div id="navbar" className=" px-3 mdp:px-8 py-3 mdp:py-5 flex items-center justify-between absolute top-0 left-0 w-full">
+      <div id="navbar" className="z-20 backdrop-filter backdrop-blur-lg bg-black mdp:bg-white bg-opacity-5 mdp:bg-opacity-50 px-3 mdp:px-8 py-3 mdp:py-5 flex items-center justify-between fixed top-0 left-0 w-full">
         <div id="navbar-menu" className="flex flex-1 mdp:hidden items-center ">
           <div className="flex w-10 h-10 rounded-lg bg-white items-center justify-center ">
             <Hamburger 
@@ -28,7 +32,7 @@ function App() {
         </div>
 
         {/* links */}
-        <div id="navbar-links" className="flex-1 hidden mdp:flex space-x-10 items-center justify-center text-base">
+        <div id="navbar-links" className="flex-1 hidden mdp:flex space-x-10 items-center justify-center text-base text-white mdp:text-gray-800">
           <a href="#">Story</a>
           <a href="#">Frogs</a>
           <a href="#">Team</a>
@@ -43,18 +47,17 @@ function App() {
           </div>
         </div>
         <div id="navbar-wallet-md" className="flex-1 hidden mdp:flex text-white">
-          <button className="w-44 h-16 border ml-auto" style={{'backgroundColor':'#01CA17'}}>
+          <button className="w-44 h-16 ml-auto" style={{'backgroundColor':'#01CA17'}}>
             Connect
           </button>
         </div>
       </div>
       {/*background image*/}
-      <div class="bg-none" className="h-0 items-center">
-        <img src="/images/landing-background.jpg" />
-        </div>
+
       {/*we need your help image*/}
-      <div id="hero" className="flex justify-center items-center h-64">
-        <img src="/images/help-landing.png" width="600" height="208"/>
+      <div id="hero" className="flex relative justify-center items-center h-screen max-h-screen" style={{'backgroundColor': '#b5d6ff'}}>
+        <img src="/images/help-landing.png" width="600" height="208" className="z-10"/>
+        <img src="/images/landing-background.jpg" className="w-full absolute bottom-0 left-0"/>
 
       </div>
 
@@ -62,24 +65,24 @@ function App() {
         
       </div>
 
-      <div id="collection" className="relative overflow-hidden flex w-full">
-      <div className="w-1/4 absolute left-0 left- flex flex-col anim-inverseScroll">
-            <img src="/images/col1.jpg" className="w-full" />
-            <img src="/images/col1.jpg" className="w-full" />
+      <div id="collection" className="relative overflow-hidden flex items-center justify-center w-full">
+        <div className="w-2/4 mdp:w-1/4 absolute left-0 top-0 flex flex-col anim-inverseScroll">
+          <img src="/images/col1.jpg" className="w-full" />
+          <img src="/images/col1.jpg" className="w-full" />
         </div>
-        <div className="w-1/4 absolute left-1/4 left- flex flex-col anim-scroll">
-            <img src="/images/col1.jpg" className="w-full" />
-            <img src="/images/col1.jpg" className="w-full" />
+        <div className="w-2/4 mdp:w-1/4 absolute left-2/4 mdp:left-1/4 top-0 flex flex-col anim-scroll">
+          <img src="/images/col1.jpg" className="w-full" />
+          <img src="/images/col1.jpg" className="w-full" />
         </div>
-        <div className="w-1/4 absolute left-2/4 left- flex flex-col anim-inverseScroll">
-            <img src="/images/col1.jpg" className="w-full" />
-            <img src="/images/col1.jpg" className="w-full" />
+        <div className="w-1/4 absolute  left-2/4 top-0 hidden mdp:flex flex-col anim-inverseScroll">
+          <img src="/images/col1.jpg" className="w-full" />
+          <img src="/images/col1.jpg" className="w-full" />
         </div>
-        <div className="w-1/4 absolute left-3/4 left- flex flex-col anim-scroll">
-            <img src="/images/col1.jpg" className="w-full" />
-            <img src="/images/col1.jpg" className="w-full" />
+        <div className="w-1/4 absolute left-3/4 top-0 hidden mdp:flex flex-col anim-scroll">
+          <img src="/images/col1.jpg" className="w-full" />
+          <img src="/images/col1.jpg" className="w-full" />
         </div>
-    
+
         <div id="collection-content" className="z-10 p-5 px-6 w-4/5 sm:w-auto bg-white">
           <h1 id="collection-content-title" className="font-bowlby text-center text-3xl">VIEW THE FROGS</h1>
           <p id="collection-content-paragraph">
@@ -91,12 +94,23 @@ function App() {
       </div>
 
       <div id="team">
+        <h1 id="team-title">THE TEAM</h1>
+        <div id="team-list">
 
+        </div>
       </div>
 
       <div id="roadmap">
 
       </div>
+
+      <div id="faq">
+
+      </div>
+
+      <footer>
+
+      </footer>
     </div>
   );
 }
