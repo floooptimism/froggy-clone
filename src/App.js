6bc5228cb9a4e844316495d73242b7590f1dca52
floooptimism <jackmord445@gmail.com>
2021-09-23 14:32:19 +0800

    modified story section

--- src/App.js
+++ src/App.js
@@ -201,24 +201,56 @@ function App() {
   );
 }
 
-export default App;
+function Story(props){
+  const [expand, setExpand] = useState(false);
+  const [inview, setInview] = useState(false);
+
+  const {observe, inView, updatePosition} = useInView({
+    threshold: 0.25,
+    onEnter: ()=>{
+      //
+    },
+    onLeave:() =>{
+      //
+    }
+,    onChange: ({ scrollDirection, observe }) => {
+      // We can also access the scroll direction from the event object
+      console.log("Scroll direction: ", scrollDirection.vertical);
+      observe();
+    }
+  })
 
 
-function Story(){
+  function collapse(){
+    setExpand(false);
+    updatePosition();
+    document.getElementById('story').style.height = '600px';
+    updatePosition();
+  }
+
+  function readmore(){
+    setExpand(true);
+    document.getElementById('story').style.height = document.getElementById('story').scrollHeight + 'px';
+    updatePosition();
+  }
+
   return (
-    <div id="story" className="px-8 py-10 flex flex-col space-y-16 items-center font-book">
+    <>
+    <div ref={observe} id="story" className="transition-all ease-in duration-300 relative px-4 py-20 flex flex-col space-y-16 items-center font-book">
       <div id="story-intro" className="flex flex-col items-center space-y-8">
         <h1 className="font-book font-bold text-4xl text-center">How our story starts...</h1>
         <img src="/images/story/story-intro.png" className="w-full h-full"/>
         <p id="paragraph">For 150 years, the hidden kingdom of Frognolia has been a wondrouds land of lush green forests, swampy lakes, and packs of Bud-weis-er. Thousands of hoppin' mad frogs live here, and their parties are legendary: the club are always jumping in Frognolia.</p>
       </div>
 
+      {inView && <h1>Working Find</h1>}
+
       <div id="story-chapter1" className="flex flex-col mdp:flex-row items-center justify-center space-x-4 space-y-8">
         <div className="flex-1">
           <img id="image" src="/images/story/story-chapter1.png" className="mx-auto"/>
         </div>
         <div className="flex-1">
-          <p id="paragraph">But leaping forwardto the present day, there's a problem. And it's a big one.<br/>
+          <p id="paragraph">But leaping forward to the present day, there's a problem. And it's a big one.<br/>
             <span className="ml-6"> </span>Storks.<br/>
             <span className="ml-6"> </span>Everywhere<br/>
             <span className="ml-6"></span>Swooping down and killing these innocent green creatures for no reason.<br/>
@@ -229,9 +261,77 @@ function Story(){
       </div>
     
       <div id="story-chapter2" className="flex flex-col item-center space-x-4 space-y-8">
-        
+        <div className="flex-1">
+            <img id="image" src="/images/story/story-chapter2.png" className="mx-auto"/>
+        </div>
+
+        <div className="flex-1">
+          <p id="paragraph">Recently, King Ribbit - the most ambitious of the amphibians - called a meeting with the High Council, the greates minds in Frognolia. King Ribbit needed a solution and quick.<br/>
+            <span className="ml-6"> </span>He laid out a table of snacks: bowls of dead spiders and chunky flies.<br/>
+            <span className="ml-6"> </span>"So," King Ribbit said, "how can we stop these storks killing everyone in FN?"<br/>
+            <span className="ml-6"></span>Dr Toady Newton put his hand up. "Blah blah blah blah<br/>
+            <span className="ml-6"></span>Lorem ipsum super super nice<br/>
+            <span className="ml-6"></span>omg shut up
+          </p>
+        </div>
       </div>
 
+      <div id="story-chapter3" className="flex flex-col mdp:flex-row items-center justify-center space-x-4 space-y-8">
+        <div className="flex-1">
+          <img id="image" src="/images/story/story-chapter3.png" className="mx-auto"/>
+        </div>
+        <div className="flex-1">
+          <p id="paragraph">But leaping forward to the present day, there's a problem. And it's a big one.<br/>
+            <span className="ml-6"> </span>Storks.<br/>
+            <span className="ml-6"> </span>Everywhere<br/>
+            <span className="ml-6"></span>Swooping down and killing these innocent green creatures for no reason.<br/>
+            <span className="ml-6"></span>I mean, it's bad.<br/>
+            <span className="ml-6"></span>Frogs are croaking left, right, and center.
+          </p>
+        </div>
+      </div>
+
+      <div id="story-chapter4" className="flex flex-col mdp:flex-row items-center justify-center space-x-4 space-y-8">
+        <div className="flex-1">
+          <img id="image" src="/images/story/story-chapter4.png" className="mx-auto"/>
+        </div>
+      </div>
+
+      <div id="story-readmore" className={`z-20 absolute left-0 w-full bottom-10 text-center ${expand ? 'hidden':''}`}>
+        {!expand && <button className="text-2xl" onClick={readmore}>Read more</button>}
+      </div>
+
+
+      <div id="button-fade" className="z-10 w-full absolute bottom-0 text-center" >
+        <div className="h-16 w-full" style={{
+        'backgroundImage':'linear-gradient(rgb(255 255 255 / 1%), rgb(219 222 238))'
+      }}>
+        </div>
+        <div className="h-16" style={{'backgroundColor':'rgb(219,222,238)'}}></div>
+      </div>
+
+      <div id="stop-reading" className={`w-full h-16 fixed bottom-8 justify-center ${expand ? 'flex':'hidden'} ${inView ? '':'movedown'}`}>
+        <button className="bg-red-500 rounded-full w-16 h-16 flex items-center justify-center"
+          onClick={collapse}
+        >
+          <svg viewBox="0 0 44 44" xmlns="http://www.w3.org/2000/svg"
+            className="w-10 h-10"
+            style={{
+              "fill":"white"
+            }}
+          >
+            <defs/>
+            <path className="cls-1" id="Close" d="M893,1243a22,22,0,1,0,22,22A22.025,22.025,0,0,0,893,1243Zm0,41a19,19,0,1,1,19-19A19,19,0,0,1,893,1284Zm8.039-27.08a1.5,1.5,0,0,0-2.12.04l-5.688,5.92-6.191-5.96a1.5,1.5,0,1,0-2.08,2.16l6.149,5.92-6.149,5.92a1.5,1.5,0,0,0-.041,2.12,1.48,1.48,0,0,0,1.081.46,1.5,1.5,0,0,0,1.04-.42l6.191-5.96,5.688,5.92a1.5,1.5,0,0,0,2.12.04,1.5,1.5,0,0,0,.042-2.12l-5.729-5.96,5.729-5.96A1.5,1.5,0,0,0,901.039,1256.92Z" transform="translate(-871 -1243)"/>
+          </svg>
+        </button>
+
+      </div>
     </div>
+    </>
   )
 }
+
+
+export default App;
+
+
